aad2fc4efc7d3fa7a183ac05ff2c3b97113a3948
soufiane-dahani <dahanissoufiane@gmail.com>
2025-05-02 17:23:18 +0100

    The .xpm should be at the end of the path

--- parsing/validate_configuration_lines.c
+++ parsing/validate_configuration_lines.c
@@ -6,7 +6,7 @@
 /*   By: sodahani <sodahani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 16:48:35 by sodahani          #+#    #+#             */
-/*   Updated: 2025/05/01 23:41:16 by sodahani         ###   ########.fr       */
+/*   Updated: 2025/05/02 17:16:49 by sodahani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,14 +53,46 @@ int	is_empty_line(char *str)
 	else
 		return (0);
 }
+int	search_xpm(char *file_path)
+{
+	int	len;
+	char *str;
+
+	if (!file_path)
+		return (0);
+	str = ft_strtrim(file_path, "\n\r\t");
+	len = ft_strlen(str);
+	if (len < 4)
+		return (0);
+	return (str[len - 4] == '.' &&
+			str[len - 3] == 'x' &&
+			str[len - 2] == 'p' &&
+			str[len - 1] == 'm');
+}
+
+void	check_xpm(t_game *game)
+{
+	if (!search_xpm(game->north_texture) ||
+		!search_xpm(game->east_texture) ||
+		!search_xpm(game->west_texture) ||
+		!search_xpm(game->south_texture))
+	{
+		ft_putstr_fd("Error: \nInvalid .xpm texture file extension\n", 2);
+		ft_malloc(0, FT_CLEAR);
+		exit(1);
+	}
+}
+
 
 void	validate_configuration_lines(t_game *game)
 {
 	int	config_count;
 	int	i;
+	// int j;
 
 	config_count = 0;
 	i = 0;
+	// j = 1;
 	while (config_count < 6 && game->map[i])
 	{
 		if (is_empty_line(game->map[i]))
@@ -71,4 +103,11 @@ void	validate_configuration_lines(t_game *game)
 		process_config_line(game, &config_count, game->map[i]);
 		i++;
 	}
+	check_xpm(game);
+	// if (config_count != 6)
+	// {
+	// 	ft_putstr_fd("Error: \nDuplicates\n", 2);
+	// 	ft_malloc(0, FT_CLEAR);
+	// 	exit(1);
+	// }
 }
