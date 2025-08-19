08e4cb07ff61c1a3777db5fc83c147f23b83fc48
Kooneo <husa.bakour02@gmail.com>
2025-08-19 20:26:06 +0100

    fixed norms

--- raycasting/texture_utils.c
+++ raycasting/texture_utils.c
@@ -6,57 +6,52 @@
 /*   By: zbakour <zbakour@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 16:50:00 by zbakour           #+#    #+#             */
-/*   Updated: 2025/08/19 17:59:42 by zbakour          ###   ########.fr       */
+/*   Updated: 2025/08/19 20:25:44 by zbakour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-// Get color from texture at specific coordinates
 int	get_texture_color(t_texture *texture, int x, int y)
 {
 	char	*pixel;
 	int		color;
 
 	if (x < 0 || x >= texture->width || y < 0 || y >= texture->height)
-		return (0x000000); // Return black if out of bounds
+		return (0x000000);
 	pixel = texture->addr + (y * texture->line_len + x * (texture->bpp / 8));
 	color = *(int *)pixel;
 	return (color);
 }
 
-// Determine which texture to use based on wall direction
 t_texture	*get_wall_texture(t_game *game, int side, int step_x, int step_y)
 {
-	if (side == 0) // Vertical wall (hit on x-side)
+	if (side == 0)
 	{
 		if (step_x > 0)
-			return (&game->ea_texture); // East wall
+			return (&game->ea_texture);
 		else
-			return (&game->we_texture); // West wall
+			return (&game->we_texture);
 	}
 	else if (side == -99)
-	{
-		return (&game->door_closed_texture); // South wall
-	}
-	else // Horizontal wall (hit on y-side)
+		return (&game->door_closed_texture);
+	else
 	{
 		if (step_y > 0)
-			return (&game->so_texture); // South wall
+			return (&game->so_texture);
 		else
-			return (&game->no_texture); // North wall
+			return (&game->no_texture);
 	}
 }
 
-// Calculate texture coordinate x based on wall hit position
 double	get_wall_x(t_game *game, double ray_x, double ray_y, int side)
 {
 	double	wall_x;
 
 	(void)game;
-	if (side == 0) // Vertical wall (hit on x-side)
+	if (side == 0)
 		wall_x = (ray_y / TILE_SIZE) - floor(ray_y / TILE_SIZE);
-	else // Horizontal wall (hit on y-side)
+	else
 		wall_x = (ray_x / TILE_SIZE) - floor(ray_x / TILE_SIZE);
 	return (wall_x);
 }
