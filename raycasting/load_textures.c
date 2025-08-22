d89479b4068bbf0808dfe74a4378f3a189ef07f5
soufiane-dahani <dahanissoufiane@gmail.com>
2025-08-22 19:07:21 +0100

    leaks in load images

--- raycasting/load_textures.c
+++ raycasting/load_textures.c
@@ -3,61 +3,79 @@
 /*                                                        :::      ::::::::   */
 /*   load_textures.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zbakour <zbakour@student.1337.ma>          +#+  +:+       +#+        */
+/*   By: sodahani <sodahani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 10:46:47 by sodahani          #+#    #+#             */
-/*   Updated: 2025/08/19 20:17:55 by zbakour          ###   ########.fr       */
+/*   Updated: 2025/08/22 19:04:38 by sodahani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-void	load_single_texture(void *mlx, t_texture *tex, char *path,
+int	load_single_texture(void *mlx, t_texture *tex, char *path,
 		char *error_msg)
 {
 	int	width;
 	int	height;
 
+	if (!path)
+	{
+		ft_putstr_fd("Texture path is NULL\n", 2);
+		return (0);
+	}
 	tex->img = mlx_xpm_file_to_image(mlx, path, &width, &height);
 	if (!tex->img)
 	{
 		ft_putstr_fd(error_msg, 2);
 		ft_putstr_fd("\n", 2);
-		ft_malloc(0, FT_CLEAR);
-		exit(1);
-	}
-	if (!path)
-	{
-		ft_putstr_fd("Texture path is NULL\n", 2);
-		ft_malloc(0, FT_CLEAR);
-		exit(1);
+		return (0);
 	}
 	tex->addr = mlx_get_data_addr(tex->img, &tex->bpp, &tex->line_len,
 			&tex->endian);
 	tex->width = width;
 	tex->height = height;
+	return (1);
+}
+
+static void	load_basic_textures(t_game *game)
+{
+	if (!load_single_texture(game->mlx, &game->no_texture,
+			game->north_texture, "Failed to load north texture"))
+		close_window(game);
+	if (!load_single_texture(game->mlx, &game->so_texture,
+			game->south_texture, "Failed to load south texture"))
+		close_window(game);
+	if (!load_single_texture(game->mlx, &game->we_texture,
+			game->west_texture, "Failed to load west texture"))
+		close_window(game);
+	if (!load_single_texture(game->mlx, &game->ea_texture,
+			game->east_texture, "Failed to load east texture"))
+		close_window(game);
+	if (!load_single_texture(game->mlx, &game->door_closed_texture,
+			game->door_closed, "Failed to load door_closed texture"))
+		close_window(game);
+}
+
+static void	load_animation_textures(t_game *game)
+{
+	if (!load_single_texture(game->mlx, &game->anim_textures[0],
+			game->anim_0, "Failed to load anim_0 texture"))
+		close_window(game);
+	if (!load_single_texture(game->mlx, &game->anim_textures[1],
+			game->anim_1, "Failed to load anim_1 texture"))
+		close_window(game);
+	if (!load_single_texture(game->mlx, &game->anim_textures[2],
+			game->anim_2, "Failed to load anim_2 texture"))
+		close_window(game);
+	if (!load_single_texture(game->mlx, &game->anim_textures[3],
+			game->anim_3, "Failed to load anim_3 texture"))
+		close_window(game);
 }
 
 void	load_textures(t_game *game)
 {
-	load_single_texture(game->mlx, &game->no_texture, game->north_texture,
-		"Failed to load north texture\n");
-	load_single_texture(game->mlx, &game->so_texture, game->south_texture,
-		"Failed to load south texture\n");
-	load_single_texture(game->mlx, &game->we_texture, game->west_texture,
-		"Failed to load west texture\n");
-	load_single_texture(game->mlx, &game->ea_texture, game->east_texture,
-		"Failed to load east texture\n");
-	load_single_texture(game->mlx, &game->door_closed_texture,
-		game->door_closed, "Failed to load door_closed texture\n");
-	load_single_texture(game->mlx, &game->anim_textures[0], game->anim_0,
-		"Failed to load anim_0 texture\n");
-	load_single_texture(game->mlx, &game->anim_textures[1], game->anim_1,
-		"Failed to load anim_1 texture\n");
-	load_single_texture(game->mlx, &game->anim_textures[2], game->anim_2,
-		"Failed to load anim_2 texture\n");
-	load_single_texture(game->mlx, &game->anim_textures[3], game->anim_3,
-		"Failed to load anim_3 texture\n");
+	load_basic_textures(game);
+	load_animation_textures(game);
 	game->anim.current_frame = 0;
 	game->anim.playing = 0;
 	game->anim.frame_delay = 0.9;
